23e74a0cdf708721baf8f50dcb9207c80dbdead1
GeraldPalominoMongea1999 <asecino1999@gmail.com>
2017-09-22 18:12:05 -0500

    borrado:       Readme.md 	borrado:       mapaDistancia.m 	borrado:       matriztheta

--- mapaDistancia.m
+++
@@ -1,9 +0,0 @@
-totalM=100;
-dijkstra_2;
-mapa=zeros(totalM,totalM);
-p=round(totalM/2)*[1 1];
-[ruta,pila]=dijkstra(p,totalM);
-for i=1:totalM*totalM
-    mapa(ruta(2,i),ruta(3,i))=ruta(1,i);
-endfor  
-imshow(mapa/(max(max(mapa))));
